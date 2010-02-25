300815128187f9ba4ccd72284049e45646a24d5e
Jonathan Weiss <jw@innerewut.de>
2010-02-25 15:03:17 +0100

    remove example

--- happening.rb
+++ happening.rb
@@ -5,26 +5,3 @@ require 'active_support'
 
 require File.dirname(__FILE__) + '/lib/aws'
 require File.dirname(__FILE__) + '/lib/s3/item'
-
-# 
-# EventMachine.run do
-#   
-#   3.times do |i|
-#     puts "scheduling #{i}"
-#     on_success = Proc.new do |http|
-#       puts "#{i} SUCCESS!"
-#       #puts http.response_header.status
-#       #puts http.response_header
-#       #puts http.response
-#     end
-#   
-#     on_error = Proc.new do |http|
-#       puts "#{i} ERROR!"
-#     end
-#   
-#     item = EventS3::Item.new(i, :server => 'heise.de', :bucket => 'newsticker', :on_success => on_success, :on_error => on_error)
-#   
-#     item.get
-#   end
-# end
-
